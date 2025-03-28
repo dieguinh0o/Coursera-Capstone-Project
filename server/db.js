635ea3149eb6d7ad55ad6009274dfc344f8ccd15
dieguinh0o <martg.diego@gmail.com>
2025-03-28 14:31:28 -0400

    Server auth setup

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<your-password>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:3rC7ilnkVHlDs89B7OLYb6r0@172.21.69.21:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
